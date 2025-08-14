85352bb32812b9c4236a47eaae68320490dcb014
chayansrk64 <chayansrk64@gmail.com>
2025-08-14 12:20:25 +0600

    problem 4 solved

--- final.js
+++ final.js
@@ -32,3 +32,19 @@ function bestTeam( player1, player2 ) {
         return "Invalid"
     }  
 }
+
+function isSame(arr1 , arr2 ) {
+     if(Array.isArray(arr1) && Array.isArray(arr2)){
+            if(arr1.length !== arr2.length){
+                return false;
+        }
+        for(let i = 0; i < arr1.length; i++){
+            if(arr1[i] !== arr2[i]){
+                return false;
+            }
+        }
+        return true;
+        }else{
+            return "Invalid"
+        }
+}
